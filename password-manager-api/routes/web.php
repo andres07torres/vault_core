0bc15d95214f7a04873c9c24f1264f39bfce2248
andres07torres <andres07torresg@gmail.com>
2026-01-19 23:28:05 -0500

    fix: añadir ruta de migración correctamente

--- password-manager-api/routes/web.php
+++ password-manager-api/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
 require __DIR__.'/auth.php';
 
 Route::get('/ejecutar-migracion', function () {
-    Artisan::call('migrate:fresh --force');
-    return "¡Tablas creadas exitosamente en TiDB Cloud!";
+    try {
+        Artisan::call('migrate:fresh --force');
+        return "¡Tablas creadas exitosamente en TiDB Cloud!";
+    } catch (\Exception $e) {
+        return "Error al migrar: " . $e->getMessage();
+    }
 });
