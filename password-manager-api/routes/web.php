650b8ad2cd2e823bfab85d62b4c2c202b491683d
andres07torres <andres07torresg@gmail.com>
2026-01-19 23:25:20 -0500

    feat: ruta temporal para ejecutar migraciones

--- password-manager-api/routes/web.php
+++ password-manager-api/routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/ejecutar-migracion', function () {
+    Artisan::call('migrate:fresh --force');
+    return "¡Tablas creadas exitosamente en TiDB Cloud!";
+});
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/ejecutar-migracion', function () {
+    Artisan::call('migrate:fresh --force');
+    return "¡Tablas creadas exitosamente en TiDB Cloud!";
+});
